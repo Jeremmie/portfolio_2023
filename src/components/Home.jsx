b72c03bf3d30791725cc1527adc4634a9b6c06e1
Jeremmie <jeremie@margouillat.net>
2023-08-20 23:18:48 +0200

    general

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ export default function Home({toggleBetweenMode}){
     return (<>
         <div className="h-full w-full flex flex-col justify-center items-center md:absolute">
         <div className="h-full absolute left-0 z-10 px-2">
-            <h1 className="absolute top-0 bg-gunmetal z-0">HELLO</h1>
+            <h1 className="absolute top-0 z-0">HELLO</h1>
             <p>bienvenu🖐️</p>
         </div>
         </div>
