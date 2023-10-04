105d937c0bc69b32c2462ccdad7447acbf2595ad
Jérémie <jeremjao@gmail.com>
2023-10-04 20:19:45 +0200

    icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -77,16 +77,11 @@ export default function Header({ switchBetweenMode, toggleBetweenMode, clickHous
         var menuVisible = document.querySelector('#menu');
         menuVisible.classList.toggle('menu_animation');
     }
-    const reloadPage = () => {
-        document.location.reload(true)
-    }
-
 
     return (<>
         <div ref={menuButton} onClick={openMenu} className="w-14 md:w-10 p-2 md:p-1.5 shadow-warm rounded-full h-fit block absolute sticky top-5 left-5 z-20 bg-melon md:bg-melon/50 md:backdrop-blur-xl dark:bg-gunmetal dark:shadow-cold transition_darkmode"><MenuBurger className="w-3 md:w-8 h-fit sticky" /></div>
         <div className="
         flex justify-start items-center h-screen z-50 fixed
-   
         ">
 
             <div id="menu" className="
@@ -102,7 +97,9 @@ export default function Header({ switchBetweenMode, toggleBetweenMode, clickHous
                 </button>
                 <button onClick={contactFull} className="my-5 px-5 py-3 rounded-full  transition_manual ">
 
-                    <svg className="w-6 fill-gunmetal dark:fill-melon transition_darkmode" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"> {/*Font Awesome Pro 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc.*/} <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" /></svg>
+                    <svg className="w-6 fill-gunmetal dark:fill-melon transition_darkmode" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512">
+                        {/* <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
+                        <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" /></svg>
                 </button>
                 <button onClick={liensFull} className="my-5 px-5 py-3 rounded-full  transition_manual ">
 
