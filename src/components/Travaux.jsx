600197db2632edd2fe356c86b9f0e9c3d6ba44cc
Jeremmie <jeremie@margouillat.net>
2023-10-14 13:36:40 +0200

    travaux

--- src/components/Travaux.jsx
+++ src/components/Travaux.jsx
@@ -27,8 +27,13 @@ export default function Travaux({ clickInHouse }) {
             </div>
         </div>
 
+        
+
         <div className='w-screen h-screen fixed bg-melon dark:bg-gunmetal '>
             <div className='overflow-auto rounded-tl-[40px] rounded-tr-[20px] w-screen mt-20 h-screen'>
+                <div className='flex flex-col justify-center my-5 items-center'>
+                    <iframe className='shadow-lg w-[90%] h-[19vh] rounded-lg ' src="https://www.youtube-nocookie.com/embed/eeH90wwWKM8?si=S6GaKqYifEUS7jvx" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                </div>
                 <div className='px-5'>
                     <h1>Highlighted projects</h1>
                     <hr className='mb-5 border-gunmetal dark:border-melon transition_darkmode' />
@@ -37,32 +42,30 @@ export default function Travaux({ clickInHouse }) {
                     <div onClick={popUpLeon} className='h-[40vh] inline-block shadow-lg mb-5 text-melon dark:bg-melon ml-5 w-[65%] dark:text-gunmetal bg-gunmetal rounded-xl transition_darkmode'>
                         <div className='bg-leonAscii bg-center bg-cover h-2/3 rounded-t-lg w-full'></div>
                         <p className='text-sm ml-2 mt-2'>charachter design</p>
-                        <p className=' text-3xl ml-2 mt-2'>Leon</p>
+                        <p className=' text-2xl ml-2 mt-2'>Leon</p>
                     </div>
 
                     <div className='h-[40vh] inline-block mb-5 shadow-lg text-melon dark:bg-melon  ml-5 w-[65%] dark:text-gunmetal bg-gunmetal rounded-xl transition_darkmode'>
                         <div className='bg-galaxyGroove bg-center bg-cover h-2/3 rounded-t-lg w-full'></div>
                         <p className='text-sm ml-2 mt-2'>album cover</p>
-                        <p className=' text-3xl ml-2 mt-2'>Galaxy Groove</p>
+                        <p className=' text-2xl ml-2 mt-2'>Galaxy Groove</p>
                     </div>
 
                     <div className='h-[40vh] inline-block mb-5 shadow-lg text-melon dark:bg-melon  ml-5 w-[65%] dark:text-gunmetal bg-gunmetal rounded-xl transition_darkmode mr-5'>
                         <div className='bg-loop bg-center bg-cover h-2/3 rounded-t-lg w-full'></div>
                         <p className='text-sm ml-2 mt-2'>3D animation</p>
-                        <p className=' text-3xl ml-2 mt-2'>Loop</p>
+                        <p className=' text-2xl ml-2 mt-2'>Loop</p>
                     </div>
 
 
                 </div>
-                <div className='flex flex-col justify-center items-center'>
-                    <iframe className='shadow-lg w-[90%] h-[19vh] rounded-lg ' src="https://www.youtube-nocookie.com/embed/eeH90wwWKM8?si=S6GaKqYifEUS7jvx" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-                </div>
+
 
                 <a href="https://jeremmie.github.io/rapport_de_stage_main/dist/index.html" target='blank'>
                     <div className='flex flex-col justify-center items-center mt-5'>
                         <div className='w-[90%] h-[19vh] bg-rapportStage bg-center bg-cover flex flex-col justify-end shadow-lg rounded-lg'>
                             <div className='flex flex-row items-center bg-black/80 rounded-b-lg'>
-                                <p className=' text-melon p-2'>internship report</p>
+                                <p className=' text-melon p-2'>2023 internship report</p>
                                 <svg className='fill-melon ml-5' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
                                     {/* <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
                                     <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" /></svg>
@@ -77,12 +80,12 @@ export default function Travaux({ clickInHouse }) {
                 </div>
 
                 {/* video game / nature morte */}
-                <div className='overflow-x-scroll whitespace-nowrap flex justify-center rounded-lg'>
+                <div className='overflow-x-scroll whitespace-nowrap flex justify-center rounded-b-lg'>
                     <div className='flex justify-between w-[90vw]'>
 
                         <a href="https://jeremmie.github.io/galaxy_generator_animate-build-/" target='blank'>
                             <div className='bg-threejs bg-center bg-cover aspect-square shadow-lg rounded-lg w-[42vw] flex items-end mb-5'>
-                                <div className='h-fit dark:bg-melon/90 bg-gunmetal/90 text-melon dark:text-gunmetal rounded-b-lg w-full transition_darkmode'>
+                                <div className='h-fit dark:bg-melon/90 bg-gunmetal/90 text-melon dark:text-gunmetal rounded-lg w-full transition_darkmode'>
                                     <p className='text-sm ml-2 mt-2'>threeJS</p>
                                     <div className='flex flex-row items-center w-full justify-between pr-5'>
                                         <p className=' text-2xl ml-2 mb-2'>Galaxy</p>
@@ -93,6 +96,8 @@ export default function Travaux({ clickInHouse }) {
                                 </div>
                             </div>
                         </a>
+
+                        
 
                         <div className='bg-asciiCube bg- bg-center bg-cover aspect-square shadow-lg rounded-lg w-[42vw] flex items-end mb-5'>
                             <div className='h-fit dark:bg-melon/90 bg-gunmetal/90 text-melon dark:text-gunmetal rounded-b-lg w-full transition_darkmode'>
