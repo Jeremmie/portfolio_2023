2689805bb334b95cf48c3aedfb7ae5c9ae06d5e2
Jeremmie <jeremie@margouillat.net>
2023-10-14 14:39:31 +0200

    Travauv horizontal slider

--- src/components/Travaux.jsx
+++ src/components/Travaux.jsx
@@ -14,9 +14,9 @@ export default function Travaux({ clickInHouse }) {
     return (<>
         <div id='popUpLeon' className='fixed p-4 bg-melon text-gunmetal -bottom-full w-screen h-screen z-50 transition_pop-up'>
             <button onClick={popUpLeon} className='block ml-auto mb-4'>
-            <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 384 512">
-            {/* <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
-                <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 384 512">
+                    {/* <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
+                    <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" /></svg>
             </button>
             <div className='overflow-auto h-full rounded-xl'>
                 <p className='text-gunmetal/70 text-l mb-3'>2022</p>
@@ -27,7 +27,7 @@ export default function Travaux({ clickInHouse }) {
             </div>
         </div>
 
-        
+
 
         <div className='w-screen h-screen fixed bg-melon dark:bg-gunmetal '>
             <div className='overflow-auto rounded-tl-[40px] rounded-tr-[20px] w-screen mt-20 h-screen'>
@@ -80,32 +80,51 @@ export default function Travaux({ clickInHouse }) {
                 </div>
 
                 {/* video game / nature morte */}
-                <div className='overflow-x-scroll whitespace-nowrap flex justify-center rounded-b-lg'>
-                    <div className='flex justify-between w-[90vw]'>
+                <div className='flex justify-end'>
+                    <div className='w-[95vw]'>
+                        <div className='bg-slate-700 w-fit px-4 shadow-lg rounded-t-lg ml-5'><p className='text-base text-melon'>web</p></div>
+                        <div className='overflow-auto shadow-lg whitespace-nowrap py-3 bg-slate-700 py-4 pl-3 rounded-l-lg mb-5'>
+                            <div className='inline-block mr-5'>
+                                <a className='inline-block' href="https://jeremmie.github.io/galaxy_generator_animate-build-/" target='blank'>
+                                    <div className='bg-threejs bg-center bg-cover aspect-square shadow-lg rounded-lg w-[42vw] flex items-end'>
+                                        <div className='h-fit dark:bg-melon/90 bg-gunmetal/90 text-melon dark:text-gunmetal rounded-lg w-full transition_darkmode'>
+                                            <p className='text-sm ml-2 mt-2'>threeJS</p>
+                                            <div className='flex flex-row items-center w-full justify-between pr-5'>
+                                                <p className=' text-2xl ml-2 mb-2'>Galaxy</p>
+                                                <svg className='fill-gunmetal ml-5' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                                                    {/* <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
+                                                    <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" /></svg>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
 
-                        <a href="https://jeremmie.github.io/galaxy_generator_animate-build-/" target='blank'>
-                            <div className='bg-threejs bg-center bg-cover aspect-square shadow-lg rounded-lg w-[42vw] flex items-end mb-5'>
-                                <div className='h-fit dark:bg-melon/90 bg-gunmetal/90 text-melon dark:text-gunmetal rounded-lg w-full transition_darkmode'>
-                                    <p className='text-sm ml-2 mt-2'>threeJS</p>
-                                    <div className='flex flex-row items-center w-full justify-between pr-5'>
-                                        <p className=' text-2xl ml-2 mb-2'>Galaxy</p>
-                                        <svg className='fill-gunmetal ml-5' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
-                                            {/* <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
-                                            <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" /></svg>
+                            <div className='inline-block mr-5'>
+                                <div className='bg-asciiCube inline-block bg-center bg-cover aspect-square shadow-lg rounded-lg w-[42vw] flex items-end'>
+                                    <div className='h-fit dark:bg-melon/90 bg-gunmetal/90 text-melon dark:text-gunmetal rounded-b-lg w-full transition_darkmode'>
+                                        <p className='text-sm ml-2 mt-2'>shader</p>
+                                        <p className=' text-2xl ml-2 mb-2'>Ascii</p>
                                     </div>
                                 </div>
                             </div>
-                        </a>
 
-                        
-
-                        <div className='bg-asciiCube bg- bg-center bg-cover aspect-square shadow-lg rounded-lg w-[42vw] flex items-end mb-5'>
-                            <div className='h-fit dark:bg-melon/90 bg-gunmetal/90 text-melon dark:text-gunmetal rounded-b-lg w-full transition_darkmode'>
-                                <p className='text-sm ml-2 mt-2'>shader</p>
-                                <p className=' text-2xl ml-2 mb-2'>Ascii</p>
+                            <div className='inline-block mr-5 '>
+                                <a href="https://jeremmie.github.io/galaxy_generator_animate-build-/" target='blank'>
+                                    <div className='bg-gameboy bg-center bg-cover aspect-square shadow-lg rounded-lg w-[42vw] flex items-end'>
+                                        <div className='h-fit dark:bg-melon/90 bg-gunmetal/90 text-melon dark:text-gunmetal rounded-b-lg w-full transition_darkmode'>
+                                            <p className='text-sm ml-2 mt-2'>threeJS</p>
+                                            <div className='flex flex-row items-center w-full justify-between pr-5'>
+                                                <p className=' text-2xl ml-2 mb-2'>Gameboy</p>
+                                                <svg className='fill-gunmetal ml-5' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                                                    {/* <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
+                                                    <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" /></svg>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
                         </div>
-
                     </div>
                 </div>
 
