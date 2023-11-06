4001534ab640a16f7176eff6010d00694b33c11c
Jérémie <jeremjao@gmail.com>
2023-11-06 19:02:59 +0100

    travaux wip

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,20 +2,20 @@ import React from "react"
 import { Canvas } from "@react-three/fiber"
 import HomeCanvas from "./HomeCanvas"
 
-export default function Home({toggleBetweenMode}){
+export default function Home({ toggleBetweenMode }) {
 
 
     return (<>
         <div className="h-full w-full flex flex-col justify-center items-center md:absolute">
-        <div className="h-fit absolute left-0 z-10 px-2">
+            <div className="h-fit absolute left-0 z-10 px-2">
 
-        </div>
+            </div>
         </div>
         <div className="h-full w-full fixed">
             <Canvas>
                 <HomeCanvas className='touch-none' toggleBetweenMode={toggleBetweenMode} />
             </Canvas>
         </div>
-        
+
     </>)
 }
