e04602ffd7e2eb58ad385fc38626759dc8e43b26
Jérémie <jeremjao@gmail.com>
2024-02-15 15:30:59 +0100

    test run deploy

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio_2023/",
-  path: "/BASE/",
+  // base: "/portfolio_2023/",
   server: {
     host: true
   }
